75b89c12ac776cba0c58057a8ad814878eb00275
Patricia-Berges <patry37.zgz@gmail.com>
2024-08-21 18:15:30 +0000

    Made function for randomizing array element, code is cleaner

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
+function getRandomElement(array) {
+  let randomElement = array[Math.floor(Math.random() * array.length)];
+  return randomElement;
+}
+
 window.onload = function() {
   let who = ["My dog", "My grandma", "The mailman", "A random bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
   let when = [
-  "before the class",
-  "yesterday",
-  "when I was sleeping",
-  "during my lunch",
-  "when I was exercising",
-  "while I was praying"
+    "before the class",
+    "yesterday",
+    "when I was sleeping",
+    "during my lunch",
+    "when I was exercising",
+    "while I was praying"
   ];
-  let random_who = who[Math.floor(Math.random() * who.length)] + " ";
-  let random_action = action[Math.floor(Math.random() * action.length)] + " ";
-  let random_what = what[Math.floor(Math.random() * what.length)] + " ";
-  let random_when = when[Math.floor(Math.random() * when.length)];
+  let random_who = getRandomElement(who) + " ";
+  let random_action = getRandomElement(action) + " ";
+  let random_what = getRandomElement(what) + " ";
+  let random_when = getRandomElement(when) + " ";
   let random_excuse = random_who.concat(
     random_action.concat(random_what.concat(random_when))
   );
